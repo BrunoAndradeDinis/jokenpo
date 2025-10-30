a39b12de406f7daddd1de2f884197af7b09ef5ff
Bruni <bruno.a.dinis96@gmail.com>
2025-10-30 12:38:13 -0300

    feat: add estrutura inicial da pagina principal

--- app/page.tsx
+++ app/page.tsx
@@ -2,100 +2,127 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <>
+      <header className="w-full flex items-center justify-start px-8 py-4 shadow-md gap-2">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/logo.png"
+          width={42}
+          height={42}
+          alt="Jokenpô logo"
+          title="Logo do Jokenpô"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className="text-lg font-bold">JOKENPÔ</h1>
+      </header>
+      <main className="max-w-4xl mx-auto py-10 flex flex-col items-center justify-center px-4">
+        <div className="w-full max-w-3xl rounded-3xl shadow-xl overflow-hidden">
+          <div className="bg-gradient-to-r from-blue-600 to-blue-500 text-white p-6 flex flex-col md:flex-row justify-between items-center gap-6">
+            <div className="bg-white text-gray-800 rounded-2xl px-6 py-4 flex flex-col items-center shadow-md">
+              <div className="flex items-center gap-2 mb-2">
+                <Image
+                  src="/player.png"
+                  width={32}
+                  height={32}
+                  alt="Jogador"
+                  title="Foto logo dos chapéus de palha"
+                />
+              </div>
+              <div>
+                <Image
+                  src="/pedra.png"
+                  alt="pedra"
+                  title="pedra"
+                  width={50}
+                  height={50}
+                />
+                <span className="mt-2 font-medium">Pedra</span>
+              </div>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <span className="text-3xl font-bold">VS</span>
+            <div className="bg-white text-gray-800 rounded-2xl px-6 py-4 flex flex-col items-center shadow-md">
+              <div className="relative flex items-center gap-2 mb-2">
+                <Image
+                  src="/computador.png"
+                  width={32}
+                  height={32}
+                  alt="Computador"
+                  title="Foto logo do franky"
+                />
+              </div>
+              <div>
+                <Image
+                  src="/pedra.png"
+                  alt="pedra"
+                  title="pedra"
+                  width={50}
+                  height={50}
+                />
+                <span className="mt-2 font-medium">Pedra</span>
+              </div>
+            </div>
+
+            <div className="bg-white text-gray-800 rounded-2xl p-4 text-center shadow-md">
+              <h3 className="font-semibold mb-2 text-blue-700">
+                Placar do jogo
+              </h3>
+              <p className="text-blue-600 font-semibold">👤 1</p>
+              <p className="text-red-500 font-semibold">💻 1</p>
+              <p className="text-gray-600 text-sm mt-1">Empates: 1</p>
+            </div>
+          </div>
+
+          <div className="text-center mt-6 mb-2">
+            <h2 className="text-green-600">
+              Você ganhou!{" "}
+              <span className="text-red-600">Você perdeu! Super!</span>{" "}
+              <span className="text-gray-600">Empate!</span>
+            </h2>
+          </div>
+
+          <div className="text-center mt-6 mb-2">
+            <p className="text-gray-700 mt-1">Faça sua próxima jogada:</p>
+          </div>
+
+          <div className="flex justify-center gap-6 mt-4 mb-6 flex-wrap">
+            <button className="flex flex-col items-center justify-center bg-white shadow-md rounded-2xl px-6 py-4 hover:shadow-lg transition border-4">
+              <Image
+                src="/pedra.png"
+                alt="pedra"
+                title="pedra"
+                width={50}
+                height={50}
+              />
+              <span className="mt-2 font-medium">Pedra</span>
+            </button>
+            <button className="flex flex-col items-center justify-center bg-white shadow-md rounded-2xl px-6 py-4 hover:shadow-lg transition border-4">
+              <Image
+                src="/papel.png"
+                alt="papel"
+                title="papel"
+                width={50}
+                height={50}
+              />
+              <span className="mt-2 font-medium">Papel</span>
+            </button>
+            <button className="flex flex-col items-center justify-center bg-white shadow-md rounded-2xl px-6 py-4 hover:shadow-lg transition border-4">
+              <Image
+                src="/tesoura.png"
+                alt="tesoura"
+                title="tesoura"
+                width={50}
+                height={50}
+              />
+              <span className="mt-2 font-medium">Tesoura</span>
+            </button>
+          </div>
+
+          <div className="flex justify-center pb-6">
+            <button className="bg-blue-600 text-white font-semibold px-6 py-2 rounded-full hover:bg-blue-700 transition">
+              Reiniciar Jogo
+            </button>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
